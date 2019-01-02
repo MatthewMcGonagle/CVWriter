3d93061fd5b23762f819c28b2132c09402c95507
Matthew McGonagle <mwmcgonagle@gmail.com>
2019-01-02 17:22:17 -0500

    Added command line parameters for directory and filename of .cv file.

--- src/Main.hs
+++ src/Main.hs
@@ -1,19 +1,25 @@
 module Main where
 
 import System.IO
+import System.Environment
 import CVWriter
 
 main :: IO ()
 main = do
 
-    handle <- openFile "info.cv" ReadMode
+    args <- getArgs
+    let (directory, filename) = case args of 
+                                     [] -> ("", "info.cv")
+                                     [name] -> ("", name)
+                                     [dir, name] -> (dir, name)
+    handle <- openFile (directory ++ filename) ReadMode
     contents <- hGetContents handle
     putStrLn "\nFile loaded.\n"
     let cvinfo = parseCV contents 
         latexString = text . convertCV <$> cvinfo 
         markdownString = jtext . convertCV <$> cvinfo 
     print latexString
-    case latexString of (Right str) -> writeFile "MatthewMcGonagleCV.tex" str
+    case latexString of (Right str) -> writeFile (directory ++ "MatthewMcGonagleCV.tex") str
                         (Left err) -> return () 
-    case markdownString of (Right str) -> writeFile "index.html" str
+    case markdownString of (Right str) -> writeFile (directory ++ "index.html") str
                            (Left err) -> return () 
